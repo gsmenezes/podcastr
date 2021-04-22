fb5af25a6de7fe116f814a52127efc931bff7ff2
gimenezes <menezes.s.gilda@gmail.com>
2021-04-22 00:56:12 -0300

    create episodes list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from "next";
+import Image from 'next/image'; //componente do next que posso utilizar no lugar das tags img - util para imagens que precisam de otimização - vetor não precisa por ser leve
 import {format, parseISO} from 'date-fns'; //parseISO vai converter para o Date do JS
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDuration";
+import styles from './home.module.scss';
 
 //tipagem das props - pode ser type ou interface
 type Episode = { //separado fica mais didático e de fácil entendimento
@@ -18,45 +20,105 @@ type Episode = { //separado fica mais didático e de fácil entendimento
 }
 
 type HomeProps = {
-  episodes: Episode[];//array de objetos
+  latestEpisodes: Episode[];//array de objetos
+  allEpisodes: Episode[];
 }
 
-export default function Home(props: HomeProps) {
+export default function Home({latestEpisodes, allEpisodes}) {
   return (
-    <div>
-      <h1>Index</h1>
-    </div>
-  );
-}
-
-export const getStaticProps:GetStaticProps = async () => {
-  const {data} = await api.get('episodes', {
-    params: {
-      _limit: 12,
-      _sort: 'published_at',
-      _order: 'desc'
+    <div className={styles.homepage}>
+    <section className={styles.latestEpisodes}>
+    <h2>Últimos lançamentos</h2>
+    <ul>
+    {latestEpisodes.map(episode => {
+      return (
+        <li key={episode.id}>
+        <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit="cover" />
+        <div className={styles.episodeDetails}>
+        <a href="">{episode.title}</a>
+        <p>{episode.members}</p>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+        </div>
+        <button type="button">
+        <img src="/play-darkpink.svg" alt="Tocar episódio"/>
+        </button>
+        </li>
+        )
+      })}
+      </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72}}>
+                    <Image width={120} height={120} src={episode.thumbnail} alt={episode.title} objectFit="cover" />
+                  </td>
+                  <td>
+                    <a href="">{episode.title}</a>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100}}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-darkpink.svg" alt="Tocar agora"/>
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+      </div>
+      );
     }
-  }) //params: limit = limite por página - sort published_at = por data de publicação - order desc = por ordem decrescente
 
-  const episodes = data.map(episode => {
-    return {
-      id: episode.id,
-      title: episode.title,
-      thumbnail: episode.thumbnail,
-      members: episode.members,
-      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR }), //format em date js, mostrando dia mês e ano, em português
-      duration: Number(episode.file.duration),
-      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
-      url: episode.file.url,
+    export const getStaticProps:GetStaticProps = async () => {
+      const {data} = await api.get('episodes', {
+        params: {
+          _limit: 12,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+      }) //params: limit = limite por página - sort published_at = por data de publicação - order desc = por ordem decrescente
 
+      const episodes = data.map(episode => {
+        return {
+          id: episode.id,
+          title: episode.title,
+          thumbnail: episode.thumbnail,
+          members: episode.members,
+          publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR }), //format em date js, mostrando dia mês e ano, em português
+          duration: Number(episode.file.duration),
+          durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+          description: episode.description,
+          url: episode.file.url,
+
+        }
+      })
+
+      const latestEpisodes = episodes.slice(0, 2); //pega os episódios da posição 0 ao 2 do array
+      const allEpisodes = episodes.slice(2, episodes.length); //pega todos os episódios a partir da posição 2 até o tamanho do array
+
+      return {
+        props: {
+          latestEpisodes,
+          allEpisodes
+        },
+        revalidate: 60 * 60 * 8,
+      }
     }
-  })
-
-  return {
-    props: {
-      episodes: episodes,
-    },
-    revalidate: 60 * 60 * 8,
-  }
-}
